e781fe5fc4e82eda72021462241f1d4b2d24360c
ricardococati <ricardo.cocati@gmail.com>
2022-02-28 19:45:54 -0300

    Criação do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO AUTOR (id, name) VALUES (1, 'Zé');
+INSERT INTO AUTOR (id, name) VALUES (2, 'Jão');
+INSERT INTO AUTOR (id, name) VALUES (3, 'Mané');
+INSERT INTO AUTOR (id, name) VALUES (4, 'Carlinho');
+INSERT INTO AUTOR (id, name) VALUES (5, 'Tonho');
