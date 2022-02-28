7ab6b8c4caa93ff855074362673c986e45c181b9
ricardococati <ricardo.cocati@gmail.com>
2022-02-28 18:53:31 -0300

    Primeiro commit

---
+++ src/test/java/com/gustavostone/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavostone.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
